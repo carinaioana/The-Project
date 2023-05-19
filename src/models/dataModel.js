ca1c77130dfed61b802e7b9f0f59b178a1a585ee
carinaioana <carinasrb@yahoo.com>
2023-05-19 17:16:44 +0300

    Full rest api + Db conn

--- src/models/dataModel.js
+++ src/models/dataModel.js
@@ -1,55 +1,103 @@
-let allData = require('../static/data')
 const { Pool } = require('pg');
-const { v4: uuidv4 } = require('uuid')
-const { writeDataToFile} = require('../api/utils')
+const { v4: uuidv4 } = require('uuid');
 
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
     password: 'password',
-    port: 5432, // default PostgresSQL port
+    port: 5432, // default PostgreSQL port
 });
 
 function findAll() {
-    return new Promise( (resolve,reject) =>{
-        resolve(allData)
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM data');
+            client.release();
+            resolve(result.rows);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
 
-    })
-}
 function findById(id) {
-    return new Promise( (resolve,reject) => {
-        const oneData = allData.find((d) => d.id === id)
-        resolve(oneData)
-    })
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM data WHERE id = $1', [id]);
+            client.release();
+
+            if (result.rows.length === 0) {
+                resolve(null);
+            } else {
+                resolve(result.rows[0]);
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
+
 function create(data) {
-    return new Promise((resolve, reject) => {
-        const newData = {...data, id: uuidv4()}
-        allData.push(newData)
-        writeDataToFile('../static/data.json', allData);
-        resolve(newData)
-    })
+    return new Promise(async (resolve, reject) => {
+        const { county, rate, total, females, males, paid, notpaid } = data;
+
+        try {
+            const client = await pool.connect();
+            const result = await client.query(
+                'INSERT INTO data (county, rate, total, females, males, paid, notpaid, id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+                [county, rate, total, females, males, paid, notpaid, uuidv4()]
+            );
+            client.release();
+            resolve(result.rows[0]);
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
-function update(id, data){
-    return new Promise((resolve, reject) =>{
-       const index = allData.findIndex( (p) => p.id === id)
-        allData[index] = {...data, id}
-        writeDataToFile('../static/data.json', allData)
-        resolve(allData[index])
-    })
+
+function update(id, data) {
+    return new Promise(async (resolve, reject) => {
+        const { county, rate, total, females, males, paid, notpaid } = data;
+
+        try {
+            const client = await pool.connect();
+            const result = await client.query(
+                'UPDATE data SET county=$1, rate=$2, total=$3, females=$4, males=$5, paid=$6, notpaid=$7 WHERE id=$8 RETURNING *',
+                [county, rate, total, females, males, paid, notpaid, id]
+            );
+            client.release();
+
+            if (result.rows.length === 0) {
+                resolve(null);
+            } else {
+                resolve(result.rows[0]);
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
-function remove(id){
-    return new Promise((resolve, reject) =>{
-        allData = allData.filter((d) => d.id!== id)
-        writeDataToFile('../static/data.json', allData)
-        resolve()
-    })
+
+function remove(id) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = await pool.connect();
+            await client.query('DELETE FROM data WHERE id=$1', [id]);
+            client.release();
+            resolve();
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
+
 module.exports = {
     findAll,
     findById,
     create,
     update,
-    remove
-}
+    remove,
+};
